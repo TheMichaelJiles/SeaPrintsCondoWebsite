68a77f118af7b024bd70de1aad1741be8591913e
Thomas Whaley <twhaley1@my.westga.edu>
2020-06-16 15:01:43 -0400

    Calendar form is now processed and the data is added to the database.

--- home/views.py
+++ home/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+
 from registration import forms
+from registration import validation
 
 def landing(request):
     if request.method == 'POST':
         print(request.POST)
+        was_successful = validation.process_post_data(request.POST)
+        if was_successful:
+            # Redirect to success page.
+        else:
+            # Redirect back to registration page with error shown.
     template_name = 'home/home.html'
     return render(request, template_name, {'form': forms.StayForm()})
--- registration/validation.py
+++ registration/validation.py
@@ -1,1 +1,29 @@
-def validatePostData(postdata):
+from dateutil import parser
+from .models import Stay
+
+def process_post_data(postdata):
+    name = postdata['name']
+    total_price = 350
+    phone = postdata['phone_contact_0'] + postdata['phone_contact_1']
+    email = postdata['email_contact']
+    num_guests = int(postdata['number_of_guests'])
+    in_date = parser.parse(postdata['in_date'].split('(')[0].strip())
+    out_date = parser.parse(postdata['out_date'].split('(')[0].strip())
+
+    if perform_validation(num_guests):
+        new_stay = Stay(name=name, in_date=in_date, out_date=out_date, total_price=total_price, phone_contact=phone, email_contact=email, number_of_guests=num_guests)
+        new_stay.save()
+        # send_notification_email()
+        is_successful = True
+    else:
+        is_successful = False
+    return is_successful
+
+def perform_validation(num_guests):
+    return is_num_guests_valid(num_guests)
+
+def is_num_guests_valid(num_guests):
+    return num_guests > 0
+
+def send_notification_email():
+    raise NotImplementedError()
