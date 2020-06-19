e1a9f962872d21a684340293a6875c12d90778bd
Thomas Whaley <twhaley1@my.westga.edu>
2020-06-19 13:25:47 -0400

    Refactored the python code within the registration app.

--- registration/models.py
+++ registration/models.py
@@ -1,4 +1,5 @@
 import datetime
+
 from django.db import models
 from django.core.exceptions import ValidationError
 
@@ -6,9 +7,12 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 from registration import registration as data
 
-# Create your models here.
-
 class Globals(models.Model):
+    '''
+    The globals model should only ever have one row. This row contains
+    site wide data related to registration that the registration services
+    rely on.
+    '''
     default_price_per_night = models.FloatField(default=175)
     minimum_days_of_stay = models.IntegerField(default=4)
 
@@ -16,11 +20,22 @@ class Globals(models.Model):
         return 'Site Setting'
 
     def clean(self):
+        '''
+        This is an overriden version of the clean method from models.Model.
+        The partial override ensures that no more than one row is created in
+        the table. A Globals object should never be instantiated manually. Instead,
+        it should be created through a ModelForm on the admin page.
+        '''
         super().clean()
         if Globals.objects.all().count() > 0:
             raise ValidationError(('There is already a global setting entry. Modify it instead of creating a new setting.'))
 
 class SeasonPricing(models.Model):
+    '''
+    This model is used to calculate the price of stays. The entries are added only
+    through the admin page; therefore, a SeasonPricing object should never be created manually.
+    Instead, it should be created through the use of a ModelForm.
+    '''
     start_date = models.DateField('season start date', unique=True)
     end_date = models.DateField('season end date', unique=True)
     price_per_night = models.FloatField()
@@ -29,7 +44,15 @@ class SeasonPricing(models.Model):
         return f'{str(self.start_date)} to {str(self.end_date)} : ${self.price_per_night}/night'
 
     def clean(self):
+        '''
+        This is an overriden version of the models.Model clean() method. The partial override performs
+        additional validation. It ensures that the price per night is positive. It ensures that the start
+        date for the season comes before the ending date for the season. It also ensures that the new season
+        does not conflict with an already existing season.
+        '''
         super().clean()
+        if self.price_per_night < 0:
+            raise ValidationError(('The price per night should not be less than 0.'))
         if self.start_date > self.end_date:
             raise ValidationError(('Season start date must be before or the same as the season end date.'))
         current_seasons = SeasonPricing.objects.all()
@@ -40,6 +63,17 @@ class SeasonPricing(models.Model):
                 raise ValidationError(('Season conflicts with an already existing season.'))
 
 class Address(models.Model):
+    '''
+    This model is used to store the addresses for customers that register stays.
+    Take the example address: 16 Chicken Street Carrollton, GA 30118 United States
+    The fields would correspond as follows:
+    street_number = '16'
+    route = 'Chicken Street'
+    city = 'Carrollton'
+    state = 'GA'
+    zip_code = '30118'
+    country = 'United States'
+    '''
     street_number = models.CharField(max_length=10)
     route = models.CharField(max_length=25)
     city = models.CharField(max_length=25)
@@ -51,6 +85,14 @@ class Address(models.Model):
         return f'{self.street_number} {self.route}\n{self.city}, {self.state} {self.zip_code}\n{self.country}'
 
 class Stay(models.Model):
+    '''
+    This is the model field that stores information about a stay.
+    By default, new stays are not approved. The only optional field is the
+    additional questions or concerns field. Each stay entry is linked to one
+    Address entry. When creating a Stay object manually, call the full_clean()
+    method before the save() method is called and catch any ValidationErrors to 
+    ensure the Stay is created with valid data.
+    '''
     name = models.CharField(max_length=20)
     in_date = models.DateField('check-in date', unique=True)
     out_date = models.DateField('check-out date', unique=True)
@@ -66,36 +108,56 @@ class Stay(models.Model):
         return f'{self.name}: {"APPROVED" if self.is_approved else "PENDING"}'
 
     def clean(self):
+        '''
+        This is the partial overriden version of the models.Model's clean method.
+        It performs additional validation to ensure data is sufficient. Then, once the
+        data is validated, calculates the total cost of the stay depending on the seasons.
+        '''
         super().clean()
-        start = self.in_date
-        end = self.out_date
+        self._perform_additional_validation()
+        self._set_calculated_total_price()
+    
+    def _perform_additional_validation(self):
+        self._ensure_checkout_occurs_before_checkin()
+        self._ensure_global_setting_is_set()
+        self._ensure_at_least_minimum_days_of_stay()
+        self._ensure_no_conflicts_with_existing_stays()
+        self._ensure_valid_number_of_guests()
 
-        if start >= end:
+    def _ensure_checkout_occurs_before_checkin(self):
+        if self.in_date >= self.out_date:
             raise ValidationError(('The check-in date must be before the check-out date.'))
 
-        num_days = (end - start).days
+    def _ensure_global_setting_is_set(self):
+        num_days = (self.out_date - self.in_date).days
         if Globals.objects.all().count() != 1:
             raise ValidationError(('Administrative Error: Globals not set.'))
+
+    def _ensure_at_least_minimum_days_of_stay(self):
         global_setting = Globals.objects.get(pk=1)
         if num_days < global_setting.minimum_days_of_stay:
             raise ValidationError((f'A minimum of {global_setting.minimum_days_of_stay} day(s) required.'))
 
+    def _ensure_no_conflicts_with_existing_stays(self):
         taken_dates = data.get_taken_dates()
         for x in range(0, num_days):
-            current_date = start + datetime.timedelta(days=x)
+            current_date = self.in_date + datetime.timedelta(days=x)
             if current_date in taken_dates:
                 raise ValidationError(('One or more dates of stay conflict with a current approved visit.'))
-        
+
+    def _ensure_valid_number_of_guests(self):
         if self.number_of_guests < 1:
             raise ValidationError(('There must be at least one guest.'))
-        
+
+    def _set_calculated_total_price(self):
+        num_days = (self.out_date - self.in_date).days
         self.total_price = 0
         for x in range(0, num_days):
-            current_date = start + datetime.timedelta(days=x)
-            price_for_day = self.get_rate(current_date)
+            current_date = self.in_date + datetime.timedelta(days=x)
+            price_for_day = self._get_rate(current_date)
             self.total_price += price_for_day
-    
-    def get_rate(self, date):
+
+    def _get_rate(self, date):
         price = Globals.objects.get(pk=1).default_price_per_night
         seasons = SeasonPricing.objects.all()
         for season in seasons:
