616a6f63e6a96cf94fa83a71102f45785ee308c3
Thomas Whaley <twhaley1@my.westga.edu>
2020-06-29 15:13:56 -0400

    Created review section on the landing page.

--- home/views.py
+++ home/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
 from registration import forms, utils
+from reviews import utils as review_utils
 
 def landing(request):
     if request.method == 'POST':
         result = utils.register_unapproved_stay(request.POST)
         print(result)
     template_name = 'home/home.html'
-    return render(request, template_name, {'form': forms.StayForm(), 'address_form': forms.AddressForm()})
+    # return render(request, template_name, {'form': forms.StayForm(), 'address_form': forms.AddressForm()})
+    return render(request, template_name, {'reviews': review_utils.get_top_n_reviews(3)})
