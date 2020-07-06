015b73ad72066b48c4a73d4d897e8f298fb1e546
Thomas Whaley <twhaley1@my.westga.edu>
2020-07-06 11:12:15 -0400

    Pulled navbar out into its own html file so it can be reused on other pages.

--- reviews/models.py
+++ reviews/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core import validators
+from django.core.exceptions import ValidationError
 
 from registration.models import Stay
 
@@ -7,11 +8,13 @@ import random
 
 class Review(models.Model):
     corresponding_stay = models.ForeignKey(Stay, on_delete=models.CASCADE)
+    # TODO: Change this later. Don't want to rebuild tables right now.
+    # corresponding_stay = models.OneToOneField(Stay, on_delete=models.CASCADE)
     rating = models.PositiveIntegerField(default=5, validators=[validators.MinValueValidator(1), validators.MaxValueValidator(5)])
     review_text = models.TextField(blank=True)
     is_published = models.BooleanField(default=False)
     publish_date = models.DateField(blank=True, null=True)
-    link_key = models.CharField(max_length=19, blank=True)
+    link_key = models.CharField(max_length=30, blank=True)
 
     def __str__(self):
         return f'{self.corresponding_stay.name}: {"Reviewed" if self.is_published else "Not Reviewed"}'
@@ -19,3 +22,4 @@ class Review(models.Model):
     def clean(self):
         super().clean()
         self.link_key = str(random.getrandbits(64))
+
