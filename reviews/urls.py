243d9c3b7edd63d33f7008a366975e8713265719
Thomas Whaley <twhaley1@my.westga.edu>
2020-07-06 12:54:24 -0400

    Review page can now be navigated.

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.get_all_reviews, name='all_reviews'),
+    path('<int:page>/', views.get_all_reviews, name='all_reviews'),
     path('publish/<str:publishkey>/', views.write_review, name="write_review"),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -3,9 +3,28 @@ from django.shortcuts import render, redirect
 from reviews import forms, utils
 from reviews.models import Review
 
-def get_all_reviews(request):
-    latest_review_list = Review.objects.filter(is_published=True).order_by('publish_date')
-    return render(request, 'reviews/reviews.html', {'latest_review_list': latest_review_list})
+REVIEWS_PER_PAGE = 5
+
+def get_all_reviews(request, page):
+    all_reviews = utils.get_all_reviews()
+    max_pages = int((all_reviews.count() / REVIEWS_PER_PAGE) + 1)
+    if page > max_pages:
+        target_page = max_pages
+    elif page < 1:
+        target_page = 1
+    else:
+        target_page = page
+    start_index = REVIEWS_PER_PAGE * (target_page - 1)
+    end_index = min((start_index + REVIEWS_PER_PAGE), all_reviews.count())
+    review_list = all_reviews[start_index:end_index]
+    return render(request, 'reviews/reviews.html', {
+        'review_list': review_list,
+        'current_page': page,
+        'is_first_page': page == 1,
+        'is_last_page': page == max_pages,
+        'next_page': page + 1,
+        'previous_page': page - 1,
+    })
 
 def write_review(request, publishkey):
     if request.method == 'POST':
