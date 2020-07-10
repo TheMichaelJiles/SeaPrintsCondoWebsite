ef2c2a00af27da696e871f12d121118b9c16a90a
TheMichaelJiles <sjiles1@my.westga.edu>
2020-07-10 15:21:08 -0400

    Began to create review form

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from .models import Review
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column, Div
+
 class ReviewForm(forms.ModelForm):
     class Meta():
         model = Review
